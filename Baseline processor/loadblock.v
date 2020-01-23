6f85e7c34a34a99918caf74fc07377aa92f8d42a
MJ <56290324+mjneri@users.noreply.github.com>
2020-01-23 17:25:58 +0800

    Update loadblock.v
    Changed the code from assign to an always@(*) block, since Vivado generates a better RTL schematic for the always block

--- Baseline processor/loadblock.v
+++ Baseline processor/loadblock.v
@@ -11,7 +11,7 @@ module loadblock(
 	input [2:0] dm_select,
 
 	// Output loaddata (Data to be loaded to REGFILE)
-	output [31:0] loaddata
+	output reg [31:0] loaddata
 );
 
 	/*Output of BLOCKRAM DATAMEM is ALWAYS word-aligned
@@ -32,25 +32,36 @@ module loadblock(
 	parameter LBU = 3'd4;
 	parameter LHU = 3'd5;
 
-	assign loaddata = 	(dm_select == LB)? (
-							(byte_offset == 2'd1)? { {24{data[15]}}, data[15:8] } :
-							(byte_offset == 2'd2)? { {24{data[23]}}, data[23:16] } :
-							(byte_offset == 2'd3)? { {24{data[31]}}, data[31:24] } :
-							{ {24{data[7]}}, data[7:0] } )					:
-
-						(dm_select == LBU)? (
-							(byte_offset == 2'd1)? { 24'd0, data[15:8] } 	:
-							(byte_offset == 2'd2)? { 24'd0, data[23:16] } 	:
-							(byte_offset == 2'd3)? { 24'd0, data[31:24] } 	:
-							{ 24'd0, data[7:0] } )							:
-
-						(dm_select == LH)? (
-							(byte_offset == 2'd2)? { {16{data[31]}}, data[31:16] } :
-							{ {16{data[15]}}, data[15:0] }	)				:
-
-						(dm_select == LHU)? (
-							(byte_offset == 2'd2)? { 16'd0, data[31:16] } 	:
-							{ 16'd0, data[15:0] }	)						:
-
-						data[31:0];
+	always@(*) begin
+		case(dm_select)
+			LB:
+				case(byte_offset)
+					2'd1: loaddata = { {24{data[15]}}, data[15:8] };
+					2'd2: loaddata = { {24{data[23]}}, data[23:16] };
+					2'd3: loaddata = { {24{data[31]}}, data[31:24] };
+					default: loaddata = { {24{data[7]}}, data[7:0] };
+				endcase
+			LBU:
+				case(byte_offset)
+					2'd1: loaddata = { 24'd0, data[15:8] };
+					2'd2: loaddata = { 24'd0, data[23:16] };
+					2'd3: loaddata = { 24'd0, data[31:24] };
+					default: loaddata = { 24'd0, data[7:0] };
+				endcase
+			LH:
+				case(byte_offset)
+					2'd2: loaddata = { {16{data[31]}}, data[31:16] };
+					2'd3: loaddata = { {16{data[31]}}, data[31:16] };
+					default: loaddata = { {16{data[15]}}, data[15:0] };
+				endcase
+			LHU:
+				case(byte_offset)
+					2'd2: loaddata = { 16'd0, data[31:16] };
+					2'd3: loaddata = { 16'd0, data[31:16] };
+					default: loaddata = { 16'd0, data[15:0] };
+				endcase
+			default: loaddata = data[31:0];
+		endcase
+	end
+	
 endmodule
