4ad3ef8357259c6243b923ce0b62c265b35b6b03
redridao88 <59949308+redridao88@users.noreply.github.com>
2020-01-28 13:45:56 +0800

    Update test2.asm
    changed address to 0x2

--- Assembler+Testbench/For Baseline/test2.asm
+++ Assembler+Testbench/For Baseline/test2.asm
@@ -5,6 +5,7 @@
 
 #LUI
 lui x1, 0x4
+lui x20, 0x2
 
 addi x0, x0, 0
 addi x0, x0, 0
@@ -16,8 +17,8 @@ addi x11, x1, 66
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x10, 0(x0)
-sw x11, 4(x0)
+sw x10, 0(x20)
+sw x11, 4(x20)
 
 #SLTI
 slti x12, x1, -1
@@ -26,8 +27,8 @@ slti x13, x1, 14
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x12, 8(x0)
-sw x13, 12(x0)
+sw x12, 8(x20)
+sw x13, 12(x20)
 
 #SLTIU
 sltiu x10, x1, -2
@@ -36,8 +37,8 @@ sltiu x11, x1, 20
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x10, 16(x0)
-sw x11, 20(x0)
+sw x10, 16(x20)
+sw x11, 20(x20)
 
 #XORI
 xori x12, x1, 14
@@ -46,7 +47,7 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x12, 24(x0)
+sw x12, 24(x20)
 
 #ORI
 ori x10, x1, 8
@@ -55,7 +56,7 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x10, 28(x0)
+sw x10, 28(x20)
 
 #ANDI
 andi x11, x1, 12
@@ -64,7 +65,7 @@ addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x11, 32(x0)
+sw x11, 32(x20)
 
 #SLLI
 slli x12, x1, 2
@@ -73,8 +74,8 @@ slli x13, x1, 4
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x12, 36(x0)
-sw x13, 40(x0)
+sw x12, 36(x20)
+sw x13, 40(x20)
 
 #SRLI
 srli x10, x1, 1
@@ -83,8 +84,8 @@ srli x11, x1, 2
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x10, 44(x0)
-sw x11, 48(x0)
+sw x10, 44(x20)
+sw x11, 48(x20)
 
 #SRAI
 srai x12, x1, 3
@@ -93,34 +94,34 @@ srai x13, x1, 5
 addi x0, x0, 0
 addi x0, x0, 0
 
-sw x12, 52(x0)
-sw x13, 56(x0)
+sw x12, 52(x20)
+sw x13, 56(x20)
 
 #LB
-lb x10, 0(x0)
+lb x10, 0(x20)
 
 #LBU
-lbu x14, 16(x0)
+lbu x14, 16(x20)
 
 #LH
-lh x11, 2(x0)
+lh x11, 2(x20)
 
 #LHU
-lhu x13, 16(x0)
+lhu x13, 16(x20)
 
 #LW
-lw x12, 16(x0)
+lw x12, 16(x20)
 
 #SB
-sb x10, 60(x0)
-sb x14, 61(x0)
+sb x10, 60(x20)
+sb x14, 61(x20)
 
 #SH
-sh x11, 62(x0)
-sh x13, 64(x0)
+sh x11, 62(x20)
+sh x13, 64(x20)
 
 #SW
-sw x12, 68(x0)
+sw x12, 68(x20)
 
 
 addi x0, x0, 0
