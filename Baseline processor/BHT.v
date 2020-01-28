62cb7d9163347c79a3b60855a101c922981153b8
redridao88 <59949308+redridao88@users.noreply.github.com>
2020-01-28 20:43:42 +0800

    Update BHT.v
    added inputs and outputs

--- Baseline processor/BHT.v
+++ Baseline processor/BHT.v
@@ -4,7 +4,20 @@ module BHT(
 	input CLK,
 	input nrst,
 
-	
+	input [9:0] PC_if,
+	input [9:0] PC_id,
+	input [9:0] PC_exe,
+	input is_jump,
+	input is_stype,
+	input ALU_feedback,
+
+	output if_pred,
+	output exe_corr,
+	output flush,
+	output [9:0] if_PBT,
+	output [9:0] exe_PBT,
+	output [9:0] exe_CNT
+
 );
 
 
