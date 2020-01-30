ebe7f2d7ba0316eef264d7fc734d5edaf7411872
Allen Tan <altnameallen@outlook.com>
2020-01-30 21:48:05 +0800

    Compressed Commit
    Something necessary for the future. Don't worry about it.
    
    Added:
    - Compressed buffer
    - Skeleton for the compressed decoder

---
+++ Baseline processor/compressed_decoder.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+/*
+
+    Compressed Instruction Decoder
+
+    This module implements the decoder that translates the 
+    
+    The decoder is separate and will be in the ID stage instead.
+
+*/
+
+module compressed_decoder(
+    input clk,
+    input nrst,
+    input [11:0] inst,
+    output [31:0] out_inst
+    );
+
+    
+
+endmodule
