2a57328aff5104bba0dace3a59a1ab1d0197ba13
redridao88 <59949308+redridao88@users.noreply.github.com>
2020-02-03 10:05:02 +0800

    Update branch_test.asm
    added NOPs

--- Assembler+Testbench/For Baseline/branch_test.asm
+++ Assembler+Testbench/For Baseline/branch_test.asm
@@ -11,26 +11,44 @@ addi x9, x0, 3
 
 loop1:
 addi x10, x1, -1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 bne x10, x0, loop1
 
 loop2:
 addi x11, x2, -1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 beq x11, x0, loop2
 
 loop3:
 addi x12, x3, 1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 blt x12, x4, loop3
 
 loop4:
 addi x13, x6, -1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 bge x13, x5, loop4
 
 loop5:
 addi x14, x7, 1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 bltu x14, x8, loop5
 
 loop6:
 addi x15, x9, 1
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 bgeu x8, x15, loop6
 
 addi x0, x0, 0
