d2ca280159aa2a5390f19652d488795ff0aabb4b
redridao88 <59949308+redridao88@users.noreply.github.com>
2020-02-06 15:13:30 +0800

    changes to assembly code
    removed nops, introduced hazards :)

--- Assembler+Testbench/For Baseline/test2.asm
+++ Assembler+Testbench/For Baseline/test2.asm
@@ -6,15 +6,15 @@
 #LUI
 lui x1, 0x4
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 #ADDI
 addi x10, x0, 66
 addi x11, x1, 66
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x10, 0(x0)
 sw x11, 4(x0)
@@ -23,8 +23,8 @@ sw x11, 4(x0)
 slti x12, x1, -1
 slti x13, x1, 14
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x12, 8(x0)
 sw x13, 12(x0)
@@ -33,8 +33,8 @@ sw x13, 12(x0)
 sltiu x10, x1, -2
 sltiu x11, x1, 20
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x10, 16(x0)
 sw x11, 20(x0)
@@ -42,27 +42,27 @@ sw x11, 20(x0)
 #XORI
 xori x12, x1, 14
 
-addi x0, x0, 0
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x12, 24(x0)
 
 #ORI
 ori x10, x1, 8
 
-addi x0, x0, 0
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x10, 28(x0)
 
 #ANDI
 andi x11, x1, 12
 
-addi x0, x0, 0
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x11, 32(x0)
 
@@ -70,8 +70,8 @@ sw x11, 32(x0)
 slli x12, x1, 2
 slli x13, x1, 4
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x12, 36(x0)
 sw x13, 40(x0)
@@ -80,8 +80,8 @@ sw x13, 40(x0)
 srli x10, x1, 1
 srli x11, x1, 2
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x10, 44(x0)
 sw x11, 48(x0)
@@ -90,8 +90,8 @@ sw x11, 48(x0)
 srai x12, x1, 3
 srai x13, x1, 5
 
-addi x0, x0, 0
-addi x0, x0, 0
+#addi x0, x0, 0
+#addi x0, x0, 0
 
 sw x12, 52(x0)
 sw x13, 56(x0)
