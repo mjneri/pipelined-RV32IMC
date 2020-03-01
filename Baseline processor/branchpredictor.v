0df0e046b7e810e68553476ae3cb19b026468383
MJ <56290324+mjneri@users.noreply.github.com>
2020-03-01 19:46:25 +0800

    BHT Bugfix

--- Baseline processor/branchpredictor.v
+++ Baseline processor/branchpredictor.v
@@ -4,11 +4,11 @@
 	Branch History Table module
 	Format of each entry:
 	========================================================================
-	| Valid bit | Tag[5:0] | Branch target[9:0] | Saturating Counter [1:0] |
+	| Valid bit | Tag[6:0] | Branch target[10:0] | Saturating Counter [1:0] |
 	========================================================================
 	BHT is implemented as a 4-way Set Associative Cache with 64 entries
 	Replacement policy is FIFO
-	id_PC[9:0] = {Tag[5:0], Set[3:0]}
+	id_PC[10:0] = {Tag[6:0], Set[3:0]}
 	For selecting next PC:
 	format of selection bits: {exe_correction[1:0], if_prediction}
 		default selection: PC+4
@@ -68,20 +68,20 @@ module branchpredictor(
 	// Declaring memory for BHT
 	/*  new format of each line in reg history_table (halfword ver.)
 		========================================================================
-		| Valid bit | Tag[5:0] | Branch target[9:0] | Saturating Counter [1:0] |
-		| ht[19]    | ht[18:13]| ht[12:2]           | ht[1:0]				   |
+		| Valid bit | Tag[6:0] | Branch target[10:0] | Saturating Counter [1:0]|
+		| ht[21]    | ht[20:13]| ht[12:2]           | ht[1:0]				   |
 		========================================================================
 		Where ht = history_table
 		*MSB of tag bits = ISR_running
 	*/
-	reg [19:0] history_table [0:63];
+	reg [21:0] history_table [0:63];
 
 
 	////////////////////////////////////////////////////////////////////////////
 
 	/*IF stage 
 		What happens here:
-		- get if_PC, get set (if_PC[3:0]) and tag (if_PC[9:4]) bits 
+		- get if_PC, get set (if_PC[3:0]) and tag (if_PC[10:4]) bits 
 		- output if_prediction (1 if taken, 0 if not taken)
 			- kukunin sa ht[1]
 		- output if_PBT (nakukuha from ht[12:2]) is the predicted branch target
@@ -93,34 +93,37 @@ module branchpredictor(
 	// if_validX: the valid bit in each entry
 	// if_iseqtoX: determines if the entry contains the same tag bits from the input
 	// if_loadentry: the entry that corresponds to the input
-	wire [19:0] if_entry0, if_entry1, if_entry2, if_entry3;
-	wire if_valid0, if_valid1, if_valid2, if_valid3;
-	wire if_iseqto0, if_iseqto1, if_iseqto2, if_iseqto3;
-	wire [19:0] if_loadentry;
+	wire [21:0] if_entry [0:3];
+	wire [3:0] if_valid;
+	wire [3:0] if_iseqto;
+	reg [21:0] if_loadentry;
+	wire [3:0] if_set = if_PC[3:0];
+	wire [6:0] if_tag = if_PC[10:4];
 
-	assign if_entry0 = history_table[{if_PC[3:0], 2'b00}];
-	assign if_entry1 = history_table[{if_PC[3:0], 2'b01}];
-	assign if_entry2 = history_table[{if_PC[3:0], 2'b10}];
-	assign if_entry3 = history_table[{if_PC[3:0], 2'b11}];
+	assign if_entry[0] = history_table[{if_set, 2'b00}];
+	assign if_entry[1] = history_table[{if_set, 2'b01}];
+	assign if_entry[2] = history_table[{if_set, 2'b10}];
+	assign if_entry[3] = history_table[{if_set, 2'b11}];
 
-	assign if_valid0 = if_entry0[19];
-	assign if_valid1 = if_entry1[19];
-	assign if_valid2 = if_entry2[19];
-	assign if_valid3 = if_entry3[19];
+	assign if_valid[0] = if_entry[0][21];
+	assign if_valid[1] = if_entry[1][21];
+	assign if_valid[2] = if_entry[2][21];
+	assign if_valid[3] = if_entry[3][21];
 
-	assign if_iseqto0 = (if_entry0[18:13] == if_PC[9:4]) && if_valid0;
-	assign if_iseqto1 = (if_entry1[18:13] == if_PC[9:4]) && if_valid1;
-	assign if_iseqto2 = (if_entry2[18:13] == if_PC[9:4]) && if_valid2;
-	assign if_iseqto3 = (if_entry3[18:13] == if_PC[9:4]) && if_valid3;
+	assign if_iseqto[0] = (if_entry[0][20:13] == {ISR_running, if_tag}) && if_valid[0];
+	assign if_iseqto[1] = (if_entry[1][20:13] == {ISR_running, if_tag}) && if_valid[1];
+	assign if_iseqto[2] = (if_entry[2][20:13] == {ISR_running, if_tag}) && if_valid[2];
+	assign if_iseqto[3] = (if_entry[3][20:13] == {ISR_running, if_tag}) && if_valid[3];
 
-	wire [3:0] if_iseq;
-	assign if_iseq = {if_iseqto3, if_iseqto2, if_iseqto1, if_iseqto0};
-
-	assign if_loadentry = 	(if_iseq == 4'b1000)? if_entry3 :
-							(if_iseq == 4'b0100)? if_entry2 :
-							(if_iseq == 4'b0010)? if_entry1 :
-							(if_iseq == 4'b0001)? if_entry0 :
-							19'b0;
+	always@(*) begin
+		case(if_iseqto)
+			4'b1000: if_loadentry = if_entry[3];
+			4'b0100: if_loadentry = if_entry[2];
+			4'b0010: if_loadentry = if_entry[1];
+			4'b0001: if_loadentry = if_entry[0];
+			default: if_loadentry = 22'b0;
+		endcase
+	end
 
 	// Assign outputs
 	assign if_PBT = if_loadentry[12:2];
@@ -146,33 +149,28 @@ module branchpredictor(
 
 	reg [1:0] fifo_counter [0:15];
 
-	wire [19:0] id_entry0, id_entry1, id_entry2, id_entry3;
-	wire id_valid0, id_valid1, id_valid2, id_valid3;
-	wire id_iseqto0, id_iseqto1, id_iseqto2, id_iseqto3;
+	wire [21:0] id_entry [0:3];
+	wire [3:0] id_valid;
+	wire [3:0] id_iseqto;
 	wire [1:0] sat_counter;
-	wire [3:0] id_set;
-	wire [5:0] id_tag;
-	assign id_set = id_PC[3:0];
-	assign id_tag = id_PC[9:4];
+	wire [3:0] id_set = id_PC[3:0];
+	wire [6:0] id_tag = id_PC[10:4];
 
 	// Checking each entry within the set to see if the input is already in the table
-	assign id_entry0 = history_table[{id_set, 2'b00}];
-	assign id_entry1 = history_table[{id_set, 2'b01}];
-	assign id_entry2 = history_table[{id_set, 2'b10}];
-	assign id_entry3 = history_table[{id_set, 2'b11}];
+	assign id_entry[0] = history_table[{id_set, 2'b00}];
+	assign id_entry[1] = history_table[{id_set, 2'b01}];
+	assign id_entry[2] = history_table[{id_set, 2'b10}];
+	assign id_entry[3] = history_table[{id_set, 2'b11}];
 
-	assign id_valid0 = id_entry0[19];
-	assign id_valid1 = id_entry1[19];
-	assign id_valid2 = id_entry2[19];
-	assign id_valid3 = id_entry3[19];
+	assign id_valid[0] = id_entry[0][21];
+	assign id_valid[1] = id_entry[1][21];
+	assign id_valid[2] = id_entry[2][21];
+	assign id_valid[3] = id_entry[3][21];
 
-	assign id_iseqto0 = (id_entry0[18:13] == id_tag) && id_valid0;
-	assign id_iseqto1 = (id_entry1[18:13] == id_tag) && id_valid1;
-	assign id_iseqto2 = (id_entry2[18:13] == id_tag) && id_valid2;
-	assign id_iseqto3 = (id_entry3[18:13] == id_tag) && id_valid3;
-
-	wire [3:0] id_iseqto;
-	assign id_iseqto = {id_iseqto3, id_iseqto2, id_iseqto1, id_iseqto0};	// if id_iseq = 0, then input is not in table yet
+	assign id_iseqto[0] = (id_entry[0][20:13] == {ISR_running, id_tag}) && id_valid[0];
+	assign id_iseqto[1] = (id_entry[1][20:13] == {ISR_running, id_tag}) && id_valid[1];
+	assign id_iseqto[2] = (id_entry[2][20:13] == {ISR_running, id_tag}) && id_valid[2];
+	assign id_iseqto[3] = (id_entry[3][20:13] == {ISR_running, id_tag}) && id_valid[3];// if id_iseq = 0, then input is not in table yet
 
 	// Saturating counter default states. Branches: WNT | Jumps: ST
 	assign sat_counter = (id_is_jump)? 2'b11 : 2'b01;
@@ -196,9 +194,9 @@ module branchpredictor(
 	wire is_bltu;
 	wire is_bgeu;
 
-	wire [5:0] exe_tag;
+	wire [6:0] exe_tag;
 	wire [3:0] exe_set;
-	assign exe_tag = exe_PC[9:4];
+	assign exe_tag = exe_PC[10:4];
 	assign exe_set = exe_PC[3:0];
 
 	assign is_beq = exe_btype[5];
@@ -228,45 +226,51 @@ module branchpredictor(
 	// exe_loadentry: the entry that corresponds to the input
 	// is_pred_correct: determines if the prediction is correct
 	// exe_setoffset: determines the offset addr within the set of the entry being accessed
-	wire [19:0] exe_entry0, exe_entry1, exe_entry2, exe_entry3;
-	wire exe_valid0, exe_valid1, exe_valid2, exe_valid3;
-	wire exe_iseqto0, exe_iseqto1, exe_iseqto2, exe_iseqto3;
-	wire [19:0] exe_loadentry;
+	wire [21:0] exe_entry [0:3];
+	wire [3:0] exe_valid;
+	wire [3:0] exe_iseqto;
+	reg [21:0] exe_loadentry;
 	wire is_pred_correct;
-	wire [1:0] exe_setoffset;
+	reg [1:0] exe_setoffset;
 
-	assign exe_entry0 = history_table[{exe_set, 2'b00}];
-	assign exe_entry1 = history_table[{exe_set, 2'b01}];
-	assign exe_entry2 = history_table[{exe_set, 2'b10}];
-	assign exe_entry3 = history_table[{exe_set, 2'b11}];
+	assign exe_entry[0] = history_table[{exe_set, 2'b00}];
+	assign exe_entry[1] = history_table[{exe_set, 2'b01}];
+	assign exe_entry[2] = history_table[{exe_set, 2'b10}];
+	assign exe_entry[3] = history_table[{exe_set, 2'b11}];
 
-	assign exe_valid0 = exe_entry0[19];
-	assign exe_valid1 = exe_entry1[19];
-	assign exe_valid2 = exe_entry2[19];
-	assign exe_valid3 = exe_entry3[19];
+	assign exe_valid[0] = exe_entry[0][21];
+	assign exe_valid[1] = exe_entry[1][21];
+	assign exe_valid[2] = exe_entry[2][21];
+	assign exe_valid[3] = exe_entry[3][21];
 
-	assign exe_iseqto0 = (exe_entry0[18:13] == exe_tag) && exe_valid0;
-	assign exe_iseqto1 = (exe_entry1[18:13] == exe_tag) && exe_valid1;
-	assign exe_iseqto2 = (exe_entry2[18:13] == exe_tag) && exe_valid2;
-	assign exe_iseqto3 = (exe_entry3[18:13] == exe_tag) && exe_valid3;
+	assign exe_iseqto[0] = (exe_entry[0][20:13] == {ISR_running, exe_tag}) && exe_valid[0];
+	assign exe_iseqto[1] = (exe_entry[1][20:13] == {ISR_running, exe_tag}) && exe_valid[1];
+	assign exe_iseqto[2] = (exe_entry[2][20:13] == {ISR_running, exe_tag}) && exe_valid[2];
+	assign exe_iseqto[3] = (exe_entry[3][20:13] == {ISR_running, exe_tag}) && exe_valid[3];
 
-	// Selecting the entry
-	wire [3:0] exe_iseq;
-	assign exe_iseq = {exe_iseqto3, exe_iseqto2, exe_iseqto1, exe_iseqto0};
+	always@(*) begin
+		case(exe_iseqto)
+			4'b1000: exe_loadentry = exe_entry[3];
+			4'b0100: exe_loadentry = exe_entry[2];
+			4'b0010: exe_loadentry = exe_entry[1];
+			4'b0001: exe_loadentry = exe_entry[0];
+			default: exe_loadentry = 22'b0;
+		endcase
+	end
 
-	assign exe_loadentry =  (exe_iseq == 4'b1000)? exe_entry3 :
-							(exe_iseq == 4'b0100)? exe_entry2 :
-							(exe_iseq == 4'b0010)? exe_entry1 :
-							(exe_iseq == 4'b0001)? exe_entry0 :
-							19'h0;
-	assign exe_setoffset =  (exe_iseq == 4'b1000)? 2'h3 :
-							(exe_iseq == 4'b0100)? 2'h2 :
-							(exe_iseq == 4'b0010)? 2'h1 :
-							(exe_iseq == 4'b0001)? 2'h0 :
-							2'h0;
+	always@(*) begin
+		case(exe_iseqto)
+			4'b1000: exe_setoffset = 2'h3;
+			4'b0100: exe_setoffset = 2'h2;
+			4'b0010: exe_setoffset = 2'h1;
+			4'b0001: exe_setoffset = 2'h0;
+			default: exe_setoffset = 2'h0;
+		endcase
+	end
+
 	// Assign outputs
 	assign exe_PBT = exe_loadentry[12:2];
-	assign exe_CNI = {exe_loadentry[18:13], exe_set} + 11'b1;
+	assign exe_CNI = {exe_loadentry[19:13], exe_set} + 11'b1;
 
 	// Check if prediction is correct & output appropriate correction
 	// If sat_counter[1] and feedback are equal, then prediction is correct.
@@ -297,7 +301,7 @@ module branchpredictor(
 			fifo_counter[i] <= 2'b0;
 		end
 		for(i = 0; i < 64; i=i+1) begin
-			history_table[i] <= 20'b0;
+			history_table[i] <= 22'b0;
 		end
 	end
 	
@@ -308,7 +312,7 @@ module branchpredictor(
 				fifo_counter[i] <= 2'b0;
 			end
 			for(i = 0; i < 64; i=i+1) begin
-				history_table[i] <= 20'b0;
+				history_table[i] <= 22'b0;
 			end
 
 		end else if(en && !stall) begin
