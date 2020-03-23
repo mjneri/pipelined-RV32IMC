652478858cc31f64680eaa15f0e261ba41510c73
MJ <56290324+mjneri@users.noreply.github.com>
2020-03-23 22:33:19 +0800

    Update datamem.v
    + Changed con_addr width to 11 bits
    + added PROTOCOLMEM instantiation
    + removed FPGAIO

--- Baseline processor/datamem.v
+++ Baseline processor/datamem.v
@@ -19,87 +19,58 @@ module datamem(
 	// Inputs from protocol controllers
 	// NOTE: protocol controllers cannot read from FPGAIO
 	input [3:0] con_write,		// Similar to dm_write
-	input [9:0] con_addr,		// datamem address from protocol controller
+	input [10:0] con_addr,		// datamem address from protocol controller
 	input [31:0] con_in,		// data input from protocol controller
 
-	output reg [31:0] data_out,	// data output to the RISC-V core
+	output [31:0] data_out,	// data output to the RISC-V core
 	output [31:0] con_out		// data output to protocol controller
 );
 	
+	// Block memory outputs
+	wire [31:0] coremem_douta, coremem_doutb;
+	wire [31:0] protocolmem_douta, protocolmem_doutb;
+
+	// Determine which blockmem output to select
+	// If x_sel = 1, select PROTOCOLMEM output, else select COREMEM output
+	wire core_sel = mem_data_addr[10];
+	wire protocol_sel = con_addr[10];
+
 	// Datamem that uses BLOCKMEM from Vivado IP Catalog
 	// Blockmem generated as TRUE DUAL PORT RAM
 	// Synchronous read
 	// Addresses 0x000 - 0x3FF (Word-aligned addresses)
-	wire [31:0] blk_mem_douta;
-	blk_mem_gen_datamem BLOCKMEM(
+	blk_mem_gen_datamem COREMEM(
 		.clka(core_clk),
 		.wea(dm_write),
 		.addra(exe_data_addr[9:0]),
 		.dina(data_in),
-		.douta(blk_mem_douta),
+		.douta(coremem_douta),
+
+		.clkb(con_clk),
+		.web(4'b0),
+		.addrb(con_addr),
+		.dinb(32'b0),
+		.doutb(coremem_doutb)
+	);
+
+	// Addresses 0x400 - 0x40F	(Word-aligned addresses)
+	blk_mem_gen_protocol PROTOCOLMEM(
+		.clka(core_clk),
+		.wea(4'b0),
+		.addra(exe_data_addr[3:0]),
+		.dina(32'b0),
+		.douta(protocolmem_douta),
 
 		.clkb(con_clk),
 		.web(con_write),
-		.addrb(con_addr),
+		.addrb(con_addr[3:0]),
 		.dinb(con_in),
-		.doutb(con_out)
+		.doutb(protocolmem_doutb)
 	);
-	
-	// Memory mapping FPGA I/O to registers. (Directly mapped to registers)
-	// Word-aligned Addresses:
-	// 0x400 -> BUTTONS (FPGAIO[0])	\\ Input from BUTTONS
-	// 0x401 -> SWITCHES (FPGAIO[1])\\ Input from SWITCHES
-	// 0x402 -> LEDs (FPGAIO[2])	\\ Input from RISC-VCore
-	reg [31:0] FPGAIO [0:2];
-	integer i;
-	initial begin
-		for(i = 0; i < 3; i=i+1)
-			FPGAIO[i] <= 0;
-	end
 
-	// Writes to FPGAIO[1:0] (BTN, SW)
-	always@(posedge con_clk) begin
-		if(!nrst) begin
-			FPGAIO[1] <= 32'h0;
-			FPGAIO[0] <= 32'h0;
-		end else begin
-			FPGAIO[1] <= {29'h0, SW};
-			FPGAIO[0] <= {28'h0, BTN};
-		end
-	end
+	// Assigning data_out for the Core
+	assign data_out = core_sel? protocolmem_douta : coremem_douta;
 
-	// Reads & Writes to FPGAIO[2] (LEDs)
-	assign LED = FPGAIO[2][3:0];
-	always@(posedge con_clk) begin
-		if(!nrst)
-			FPGAIO[2] <= 0;
-		else begin
-			if(exe_data_addr == 11'h402) begin
-				case(dm_write)
-					// SW
-					4'b1111: FPGAIO[2] <= data_in;
-
-					// SH
-					4'b0011: FPGAIO[2] <= (FPGAIO[2] & 32'hffff0000) | (data_in & 32'h0000ffff);
-					4'b1100: FPGAIO[2] <= (FPGAIO[2] & 32'h0000ffff) | (data_in & 32'hffff0000);
-
-					// SB
-					4'b0001: FPGAIO[2] <= (FPGAIO[2] & 32'hffffff00) | (data_in & 32'h000000ff);
-					4'b0010: FPGAIO[2] <= (FPGAIO[2] & 32'hffff00ff) | (data_in & 32'h0000ff00);
-					4'b0100: FPGAIO[2] <= (FPGAIO[2] & 32'hff00ffff) | (data_in & 32'h00ff0000);
-					4'b1000: FPGAIO[2] <= (FPGAIO[2] & 32'h00ffffff) | (data_in & 32'hff000000);
-				endcase
-			end
-		end
-	end
-
-	// Assigning data_out
-	always@(*) begin
-		case(mem_data_addr)
-			11'h400: data_out = FPGAIO[0];
-			11'h401: data_out = FPGAIO[1];
-			11'h402: data_out = FPGAIO[2];
-			default: data_out = blk_mem_douta;
-		endcase
-	end
+	// Assigning con_out
+	assign con_out = protocol_sel? protocolmem_doutb : coremem_doutb;
 endmodule
