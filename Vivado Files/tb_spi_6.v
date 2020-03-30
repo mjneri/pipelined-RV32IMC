5c0d201d0f4b20b4129a63c68b193df1bb4cbcd9
redridao88 <59949308+redridao88@users.noreply.github.com>
2020-03-30 21:27:24 +0800

    Update tb_spi_6.v

--- Vivado Files/tb_spi_6.v
+++ Vivado Files/tb_spi_6.v
@@ -53,14 +53,95 @@ module tb_spi();
                 .dat(dat)
         );
 
+        task test_spi();
+                // inputs 
+	        input TURNON;
+	        input EN;
+	        input MB;
+	        input [23:0] PRESCALE;
+	        input [31:0] DIN;
+	        input [1:0] SEL;
+	        input CPOL;
+	        input CPHA;
+	        input ORDER;
+	        input MISO;
+
+                begin
+                        TURNON = turnon;
+                        EN = enable;
+                        MB = mb;
+                        PRESCALE = prescale;
+                        DIN = in;
+                        SEL = select;
+                        CPOL = cpol;
+                        CPHA = cpha;
+                        ORDER = order;
+                        MISO = miso;  
+
+                end
+
+
+        endtask
+
         always
                 #10 clk = ~clk;
 
+        initial begin
+        /*
+	        input TURNON;
+	        input EN;
+	        input MB;
+	        input [23:0] PRESCALE; // 4 bits onli
+	        input [31:0] DIN;
+	        input [1:0] SEL;
+	        input CPOL;
+	        input CPHA;
+	        input ORDER;
+	        input MISO;
+
+        
+        */
+
+                clk = 0;
+
+                #40 
+                #20 test_spi(0, 0, 0, {20'd0, $random%4}, $random%32, $random%2, 0, 0, 0, 0);
+                #20 test_spi(0, 0, 0, {20'd0, $random%4}, $random%32, $random%2, 0, 0, 0, 0);
+                #20 test_spi(0, 0, 0, {20'd0, $random%4}, $random%32, $random%2, 0, 1, 0, 1);
+                #20 test_spi(0, 0, 0, {20'd0, $random%4}, $random%32, $random%2, 0, 0, 1, 1);
+                #20 test_spi(1, 0, 0, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 1, 1);
+                
+                #300 test_spi(1, 0, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 1, 1);
+         
+                #50 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 0, 0);
+                #20 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 1, 0, 0);
+                #20 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 0, 0);
+                #20 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 0, 1, 0, 1);
+
+                #300 test_spi(1, 0, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 1, 1);
+         
+                #50 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 0, 0, 0);
+                #20 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 1, 0, 0);
+                #50 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 1, 0, 1);
+
+                #200 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 1, 1, 1);
+
+                #20 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 1, 1, 1, 0);
+
+                #600 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 0, 1, 1, 0);
+                #60 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 0, 1, 0, 1);
+
+                #700 test_spi(1, 1, 1, {20'd0, $random%4}, $random%32, $random%2, 0, 0, 1, 1);
+
+
+
+                #500 $finish;          
+
+
+        end
         
 
 
-
-        );
 
 
 endmodule
