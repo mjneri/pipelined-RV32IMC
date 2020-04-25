aa5e7d18f6d0184561e549b0ba8a3cc2f48648ba
MJ <56290324+mjneri@users.noreply.github.com>
2020-04-25 12:29:55 +0800

    mcont.v update
    + Module description added at beginning of file
    + UART ports changed to reflect added features
    + counter logic updated to take into account latency of blockmem
    + interrupt signals tied to protocol DONE signals (please check this for SPI & I2C; this has already been tested w/ UART)

--- Baseline processor/protocol-controllers/mcont.v
+++ Baseline processor/protocol-controllers/mcont.v
@@ -1,3 +1,20 @@
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// mcont.v -- Container for protocol controllers
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Author: Microlab 198 Single-cycle RISC-V Group (2SAY1819)
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Module Name: mcont.v
+// Description:
+//
+// Revisions:
+// Revision 0.01 - File Created
+// Revision 1.00 - File modified by Pipelined RISC-V Group (2SAY1920)
+// Additional Comments:
+// 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 `timescale 1ns/1ps
 
 module mcont(
@@ -56,7 +73,7 @@ module mcont(
 	wire		[5:0]	cnt;
 	assign	cnt	=	ctr[6:1];
 	wire mb;
-	assign uart_rco[31:7] = 25'd0;
+	assign uart_rco[31:10] = 22'd0;
 
 	spi SPI1(
 		.clk(clk),
@@ -88,25 +105,31 @@ module mcont(
 		.dat(spi_dat_t)
 	);
 	
-	Encoder UART1(
+	Encoder UART_TX1(
 		.clk(clk),
         .nrst(nrst),
-        .data_in_mem(uart_din[7:0]),
-        .wr(uart_con[0]),
+
+        .data_in(uart_din[7:0]),
+        .en(uart_con[0]),
+        .parity(uart_con[2:1]),
+        .stop_sel(uart_con[3]),
         .baudcontrol(uart_con[31:8]),
-        .parity(uart_con[1]),
-        .last_byte(uart_con[2]),
-        .data_out(uart_enc),
-        .u_rco(uart_rco[5:0])
+
+        .uart_enc(uart_enc),
+        .uart_rco(uart_rco[7:0])
     );
         
-    Decoder UART2(
+    Decoder UART_RX1(
    		.clk(clk),
    		.nrst(nrst),
-   		.data_in(uart_dec),        
+
+   		.uart_dec(uart_dec),        
    		.baudcontrol(uart_con[31:8]),    
-   		.parity(uart_con[1]),        
-   		.done(uart_rco[6]),        
+   		.parity(uart_con[2:1]),
+		.stop_sel(uart_con[3]),
+		
+   		.rddone(uart_rco[8]),
+		.perr(uart_rco[9]),
    		.data_out(uart_dat)            
     );
     
@@ -164,87 +187,93 @@ module mcont(
 		end
 		else begin
 			// the counter cycles through the data memory addresses
-			ctr		<=	ctr == 7'h17	?	7'h0	:	ctr + 7'h1;
+			ctr		<=	ctr == 7'hb	?	7'h0	:	ctr + 7'h1;
 			case(ctr) 
 				7'h0: begin
 					mem_addr	<=	11'h001;	// Read from address 0x001 (SPI Data In)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
 				end
-				7'h2: begin
+				7'h1: begin
 					mem_addr	<=	11'h002;	// Read from address 0x002 (SPI Input Control 1)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
-					spi_din		<=	mem_in;
 				end
-				7'h4: begin
+				7'h2: begin
 					mem_addr	<=	11'h401;	// Write to address 0x401 (SPI Output Control 1)
 					mem_out		<=	spi_rco_t;
 					mem_wr		<=	4'hf;
-					spi_con		<=	mem_in;
+
+					spi_din		<=	mem_in;		// SPI Data In
 				end
-				7'h6: begin
+				7'h3: begin
 					mem_addr	<=	11'h402;	// Write to address 0x402 (SPI Data Out)
 					mem_out		<=	spi_dat_t;
 					mem_wr		<=	4'hf;
+					
+					spi_con		<=	mem_in;		// SPI Input Control
 				end
-				7'h8: begin
+				7'h4: begin
 					mem_addr	<=	11'h003;	// Read from address 0x003 (UART Data In)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
 				end
-				7'ha: begin
+				7'h5: begin
 					mem_addr	<=	11'h004;	// Read from address 0x004 (UART Input Control 1)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
-					uart_din	<=	mem_in;
 				end
-				7'hc: begin
+				7'h6: begin
 					mem_addr	<=	11'h403;	// Write to address 0x403 (UART Output Control 1)
 					mem_out		<=	uart_rco;	// write only when DONE2 = 1
 					mem_wr		<=	4'hF;
-					uart_con	<=	mem_in;
+
+					uart_din	<=	mem_in;		// UART Data In 1
 				end
-				7'he: begin
+				7'h7: begin
 					mem_addr	<=	11'h404;	// Write to address 0x404 (UART Data Out)
 					mem_out		<=	uart_dat;
 					mem_wr		<=	4'hF;
+					
+					uart_con	<=	mem_in;		// UART Input Control 1
 				end
-				7'h10: begin
+				7'h8: begin
 					mem_addr	<=	11'h005;	// Read from address 0x005 (I2C Data in)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
 				end
-				7'h12: begin
+				7'h9: begin
 					mem_addr	<=	11'h006;	// Read from address 0x006 (I2C Input Control 1)
 					mem_out		<=	32'h0;
 					mem_wr		<=	4'h0;
-					i2c_din		<=	mem_in;
 				end
-				7'h14: begin
+				7'ha: begin
 					mem_addr	<=	11'h406;	// Write to address 0x406 (I2C Output control 1)
 					mem_out		<=	i2c_rco;
 					mem_wr		<=	4'hF;
-					i2c_con		<=	mem_in;
+					
+					i2c_din		<=	mem_in;		// I2C Data In		
 				end
-				7'h16: begin
+				7'hb: begin
 					mem_addr	<=	11'h405;	// Write to address 0x405 (I2C Data out)
 					mem_out		<=	i2c_dat;
 					mem_wr		<=	4'hF;
+					
+					i2c_con		<=	mem_in;		// I2C Input Control 1
 				end
-				// default: begin
-				// 	mem_addr	<=	11'h000;
-				// 	mem_out		<=	32'h0;
-				// 	mem_wr		<=	4'h0;
-				// end
+				default: begin
+					mem_addr	<=	11'h000;
+					mem_out		<=	32'h0;
+					mem_wr		<=	4'h0;
+				end
 			endcase
 		end
 	end
 
 	// Interrupt only on the following conditions:
 	// SPI: when DONE = 1 (spi_rco_t[0])
-	// UART: when DONE2 = 1 (uart_rco[6])
+	// UART: when RDDONE = 1 (uart_rco[8])
 	// I2C: when DONE = 1 (i2c_rco[1])
-	assign int_sig = (spi_rco_t[1] == 1'b1) | (uart_rco[6] == 1'b1) | (i2c_rco[1] == 1'b1);
+	assign int_sig = (spi_rco_t[1] == 1'b1) | (uart_rco[8] == 1'b1) | (i2c_rco[1] == 1'b1);
 	
 endmodule
